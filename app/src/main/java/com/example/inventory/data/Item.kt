608a86da5a22862f2edfbad14d97ec0f7265d518
unknown <serhiiromanchuk@gmail.com>
2023-01-22 13:04:44 +0200

    create item entity

---
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -0,0 +1,13 @@
+package com.example.inventory.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "items")
+data class Item (
+    @PrimaryKey @ColumnInfo(name = "id")val id: Int,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "price") val price: Int,
+    @ColumnInfo(name = "quantity") val quantity: Int
+)
