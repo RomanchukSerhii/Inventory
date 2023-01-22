db2efd48e85e4be9e8a294815dc6454a1acafadf
unknown <serhiiromanchuk@gmail.com>
2023-01-22 19:48:24 +0200

    EditProductFragment layout

---
+++ app/src/main/java/com/example/inventory/model/EditProductFragment.kt
@@ -0,0 +1,30 @@
+package com.example.inventory.model
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.inventory.R
+import com.example.inventory.databinding.FragmentEditProductBinding
+
+
+class EditProductFragment : Fragment() {
+    private var _binding: FragmentEditProductBinding? = null
+    private val binding: FragmentEditProductBinding
+        get() = _binding ?: throw RuntimeException("FragmentEditProductBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentEditProductBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
